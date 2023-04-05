21a313c21fa5c886908036ff96b33b3c0cb728de
Virginia Barbaro <virginiabarbaro@icloud.com>
2023-04-05 17:06:16 -0300

    last change

--- Blog_article-main/routes/articleRoutes.js
+++ Blog_article-main/routes/articleRoutes.js
@@ -3,10 +3,15 @@ const router = express.Router();
 const articleController = require("../controllers/articleController");
 const isAuthenticated = require("../middlewares/isAuthenticated");
 const commentController = require("../controllers/commentController");
+const adminController = require("../controllers/adminController")
+const onlyAdminCanDelete = require("../middlewares/onlyAdminCanDelete");
+const editorOnlyModifyComment = require("../middlewares/editorOnlyModifyComment")
 
 
 router.post("/comments/:id", isAuthenticated, commentController.commentPost);
 
 router.get("/:id", articleController.showSingleArticle);
+
+router.get("/comments/:id", editorOnlyModifyComment, onlyAdminCanDelete, adminController.destroyComment)
 
 module.exports = router;
